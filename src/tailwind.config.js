97de4c65529f5dc2301fe516d1d88d66f022bce7
Lawrence <Lawrence4code@gmail.com>
2020-10-19 14:40:09 +0530

    chore: basic clean cra clean up and tailwind setup

---
+++ src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  theme: {
+    fontFamily: {
+      body: ['lato'],
+    },
+  },
+  variants: {
+    fontSmoothing: ['responsive', 'hover', 'focus'],
+  },
+  plugins: [],
+};
